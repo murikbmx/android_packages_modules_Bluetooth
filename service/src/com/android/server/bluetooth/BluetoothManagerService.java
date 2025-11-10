2ff2c4359836f0ca027e30e456a7b7dab5e0d7f8
murikbmx <murikbmx@gmail.com>
2025-11-11 00:26:04 +0500

    Bluetooth timeout feature Add a bluetooth timeout setting and listen to changes to it as well as the bluetooth radio and connection state. Schedule an alarm to turn bluetooth off if it is enabled but not connected
    Issue: calyxos#124
    Change-Id: I4f4646139cd42b7ad20b486ed77dd00499aa3c6e
    (cherry picked from commit 28da7976f4643c1b9c49edcefc8f620aeaa06ae2)
    
    Issue: calyxos#830
    Change-Id: I5133909a030b5bb940707320420a26d8e1eb4852
    
    Alarm listener must not be a local variable
    
    Issue: calyxos#1050
    Change-Id: Ibb00c13ffe0fa5811c4fbd451acb8fa25fa58813
    
    Use AOSP Settings for BLUETOOTH_OFF_TIMEOUT
    
    This APEX module is not able to access LineageSettings, so the setting
    is moved back to AOSP.

--- service/src/com/android/server/bluetooth/BluetoothManagerService.java
+++ service/src/com/android/server/bluetooth/BluetoothManagerService.java
@@ -44,6 +44,8 @@ import static java.util.Objects.requireNonNull;
 
 import android.annotation.NonNull;
 import android.app.ActivityManager;
+import android.app.AlarmManager;
+import android.app.AlarmManager.OnAlarmListener;
 import android.app.BroadcastOptions;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothStatusCodes;
@@ -61,6 +63,7 @@ import android.content.ServiceConnection;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.database.ContentObserver;
 import android.os.Binder;
 import android.os.Build;
 import android.os.Bundle;
@@ -85,6 +88,7 @@ import com.android.bluetooth.flags.Flags;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.modules.expresslog.Counter;
 import com.android.modules.expresslog.Histogram;
+import com.android.modules.utils.HandlerExecutor;
 import com.android.server.bluetooth.airplane.AirplaneModeListener;
 import com.android.server.bluetooth.satellite.SatelliteModeListener;
 
@@ -183,6 +187,10 @@ class BluetoothManagerService {
     private final BluetoothServiceBinder mBinder;
     @VisibleForTesting final BluetoothHandler mHandler;
     private final ContentResolver mContentResolver;
+    
+    // Settings.Global.BLUETOOTH_OFF_TIMEOUT
+    private static final String BLUETOOTH_OFF_TIMEOUT = "bluetooth_off_timeout";
+    
     private final Context mContext;
     private final Looper mLooper;
     private final UserManager mUserManager;
@@ -540,6 +548,9 @@ class BluetoothManagerService {
                                             ? MESSAGE_RESTORE_USER_SETTING_OFF
                                             : MESSAGE_RESTORE_USER_SETTING_ON);
                         }
+                    } else if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)
+                        || BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED.equals(action)) {
+                        setBluetoothTimeout();
                     } else if (action.equals(Intent.ACTION_SHUTDOWN)) {
                         Log.i(TAG, "Device is shutting down.");
                         mShutdownInProgress = true;
@@ -558,6 +569,24 @@ class BluetoothManagerService {
             new Histogram(
                     "bluetooth.value_shutdown_latency", new Histogram.UniformOptions(50, 0, 3000));
 
+    private final OnAlarmListener mBluetoothTimeoutListener = new OnAlarmListener() {
+        @Override
+        public void onAlarm() {
+            try {
+                // Fetch adapter connection state synchronously and assume disconnected on error
+                if (mAdapter == null) return;
+                int adapterConnectionState = mAdapter.getConnectionState();
+
+                if (getState() == BluetoothAdapter.STATE_ON
+                    && adapterConnectionState == BluetoothAdapter.STATE_DISCONNECTED) {
+                    disable(mContext.getAttributionSource().getPackageName(), /*persist*/ true);
+                }
+            } catch (RemoteException e) {
+                Log.e(TAG, "setBluetoothTimeout() failed", e);
+            }
+        }
+    };
+    
     BluetoothManagerService(@NonNull Context context, @NonNull Looper looper) {
         mContext = requireNonNull(context, "Context cannot be null");
         mContentResolver = requireNonNull(mContext.getContentResolver(), "Resolver cannot be null");
@@ -590,6 +619,8 @@ class BluetoothManagerService {
         }
 
         IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED);
+        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
         filter.addAction(Intent.ACTION_SETTING_RESTORED);
         filter.addAction(Intent.ACTION_SHUTDOWN);
         filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
@@ -644,6 +675,27 @@ class BluetoothManagerService {
         Log.d(TAG, "AutoOnFeature property=" + mDeviceConfigAllowAutoOn);
         if (mDeviceConfigAllowAutoOn) {
             Counter.logIncrement("bluetooth.value_auto_on_supported");
+        }
+        
+        mContentResolver.registerContentObserver(Settings.Global.getUriFor(
+                BLUETOOTH_OFF_TIMEOUT), false,
+            new ContentObserver(null) {
+                @Override
+                public void onChange(boolean selfChange) {
+                    setBluetoothTimeout();
+                }
+            });
+    }
+
+    private void setBluetoothTimeout() {
+        long bluetoothTimeoutMillis = Settings.Global.getLong(mContext.getContentResolver(),
+            BLUETOOTH_OFF_TIMEOUT, 0);
+        AlarmManager alarmManager = mContext.getSystemService(AlarmManager.class);
+        alarmManager.cancel(mBluetoothTimeoutListener);
+        if (bluetoothTimeoutMillis != 0) {
+            final long timeout = SystemClock.elapsedRealtime() + bluetoothTimeoutMillis;
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.ELAPSED_REALTIME_WAKEUP, timeout,
+                TAG, new HandlerExecutor(mHandler), null, mBluetoothTimeoutListener);
         }
     }
 
